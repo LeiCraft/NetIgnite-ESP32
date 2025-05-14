c3684168d06ff5609be72e16994f69cb42cdeb6e
LeiCraft_ <88381277+LeiCraft@users.noreply.github.com>
2025-05-14 11:09:44 +0200

    added secrets sample

---
+++ src/secrets.sample.h
@@ -0,0 +1,11 @@
+
+#define C_WIFI_SSID "WIFI_SSID"
+#define C_WIFI_PASSWORD "WIFI_PASSWORD"
+
+#define C_WIFI_LOCAL_IP IPAddress(192, 168, 1, 32)
+#define C_WIFI_GATEWAY_IP IPAddress(192, 168, 1, 1)
+#define C_WIFI_SUBNET_MASK IPAddress(255, 255, 255, 0)
+
+// Leave 0 to use the default DNS server the router advertises
+#define C_WIFI_PRIMARY_DNS (uint32_t)0
+#define C_WIFI_SECONDARY_DNS (uint32_t)0
