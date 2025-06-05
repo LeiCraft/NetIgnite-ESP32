1e24073563cba3e6fdadf3bb199c7bfaef6a164c
LeiCraft_ <88381277+LeiCraft@users.noreply.github.com>
2025-06-05 15:27:39 +0200

    update

--- src/utils/inputValidation.h
+++ src/utils/inputValidation.h
@@ -29,6 +29,24 @@ class InputValidation {
 		return port > 0 && port <= 65535;
 	}
 
+	static bool ipv4Address(const char *ipAddress) {
+		if (!ipAddress) return false;
+
+		const char* pattern = "^(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])(\\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])){3}$";
+		return RegExp::match(ipAddress, pattern);
+	}
+
+	static bool ipv6Address(const char *ipAddress) {
+		if (!ipAddress) return false;
+
+		const char* pattern = "^(([0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}|::|(([0-9a-fA-F]{1,4}:){1,7}:)|(([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4})|(([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2})|(([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3})|(([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4})|(([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5})|([0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6}))|(:((:[0-9a-fA-F]{1,4}){1,7}|:))|(::ffff:(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])(\\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])){3}))$";
+		return RegExp::match(ipAddress, pattern);
+	}
+
+	static bool ipAddress(const char *ipAddress) {
+		return InputValidation::ipv4Address(ipAddress) || InputValidation::ipv6Address(ipAddress);
+	}
+
 };
 
 
