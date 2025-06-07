8c6d42fea044b39fe6bbcb925c2e2aa84c8aa0f2
LeiCraft_ <88381277+LeiCraft@users.noreply.github.com>
2025-06-07 11:44:58 +0200

    update

--- src/utils/icmp.h
+++ src/utils/icmp.h
@@ -4,158 +4,114 @@
 #include <Arduino.h>
 #include <IPAddress.h>
 #include <IPv6Address.h>
-#include "utils/ping.h"
-
-extern "C" void esp_schedule(void) {};
-extern "C" void esp_yield(void) {};
-
-
-#define DEBUG_PING(...) Serial.printf(__VA_ARGS__)
-
-
+#include <ping/ping_sock.h>
+#include <lwip/inet.h>
+#include <lwip/netdb.h>
+#include <net/if.h>
+#include <lwip/ip4.h>
+#include <lwip/ip6.h>
+#include <esp_netif_net_stack.h>
 
 class ICMP {
   public:
 
-    static bool ping(IPAddress dest, byte count = 5) {
-        _expected_count = count;
-        _errors = 0;
-        _success = 0;
+    static bool ping(IPAddress dest, size_t count = 5) {
 
-        _avg_time = 0;
+        ip_addr_t target_addr;
+        target_addr.type = IPADDR_TYPE_V4;
+        target_addr.u_addr.ip4.addr = static_cast<const uint32_t>(dest);
 
-        ping_option _options;
+        initialize_ping(target_addr, count);
 
-        // Repeat count (how many time send a ping message to destination)
-        _options.count = count;
-        // Time interval between two ping (seconds??)
-        _options.coarse_time = 1;
-        // Destination machine
-        _options.ip = static_cast<uint32_t>(dest);
-
-        // Callbacks
-        _options.recv_function = reinterpret_cast<ping_recv_function>(&_ping_recv_cb);
-        _options.sent_function = NULL; //reinterpret_cast<ping_sent_function>(&_ping_sent_cb);
-
-        
-        // Suspend till the process end
-        esp_yield(); // ????????? Where should this be placed?
-        
-        // Let's go!
-        ping_start(&_options); // Here we do all the work
-
-        // Returns true if at least 1 ping had a pong response 
-        return (_success > 0); //_success variable is changed by the callback function
-
+        return true;
     }
 
-    static bool ping(IPv6Address dest, byte count = 5) {
+    static bool ping(IPv6Address dest, size_t count = 5) {
         
-        _expected_count = count;
-        _errors = 0;
-        _success = 0;
+        ip_addr_t target_addr;
+        target_addr.type = IPADDR_TYPE_V6;
+        // target_addr.u_addr.ip6.zone = get_netif_index();
 
-        _avg_time = 0;
+        memcpy(target_addr.u_addr.ip6.addr, static_cast<const uint32_t*>(dest), sizeof(target_addr.u_addr.ip6.addr));
 
-        ping6_option _options;
+        initialize_ping(target_addr, count);
 
-        // Repeat count (how many time send a ping message to destination)
-        _options.count = count;
-        // Time interval between two ping (seconds??)
-        _options.coarse_time = 1;
-        // Destination machine
-        memcpy(_options.ip, static_cast<const uint8_t*>(dest), 16);
-        
-
-        // Callbacks
-        _options.recv_function = reinterpret_cast<ping_recv_function>(&_ping_recv_cb);
-        _options.sent_function = NULL; //reinterpret_cast<ping_sent_function>(&_ping_sent_cb);
-
-        
-        // Suspend till the process end
-        esp_yield(); // ????????? Where should this be placed?
-        
-        // Let's go!
-        ping6_start(&_options); // Here we do all the work
-
-        // Returns true if at least 1 ping had a pong response 
-        return (_success > 0); //_success variable is changed by the callback function
-
+        return true;
     }
 
   private:
-    static void _ping_recv_cb(void *opt, void *resp) {
-        // // Cast the parameters to get some usable info
-        // ping_resp *ping_resp = reinterpret_cast<struct ping_resp *>(resp);
-        // //ping_option* ping_opt  = reinterpret_cast<struct ping_option*>(opt);
+    static void initialize_ping(ip_addr_t target_addr, size_t count) {
 
-        // // Error or success?
-        // _errors = ping_resp->timeout_count;
-        // _success = ping_resp->total_count - ping_resp->timeout_count;
-        // _avg_time = ping_resp->resp_time;
-        
-        // // Done, return to main function
-        // esp_schedule();
+        esp_ping_config_t ping_config = ESP_PING_DEFAULT_CONFIG();
+        ping_config.target_addr = target_addr;
+        ping_config.count = count;
+        // ping_config.interface = get_netif_index();
 
-        // Cast the parameters to get some usable info
-        ping_resp *ping_resp = reinterpret_cast<struct ping_resp *>(resp);
-        //ping_option* ping_opt  = reinterpret_cast<struct ping_option*>(opt);
+        esp_ping_callbacks_t cbs;
+        cbs.on_ping_success = on_ping_success;
+        cbs.on_ping_timeout = on_ping_timeout;
+        cbs.on_ping_end = on_ping_end;
+        cbs.cb_args = NULL;
 
-        // Error or success?
-        _errors = ping_resp->timeout_count;
-        _success = ping_resp->total_count - ping_resp->timeout_count;
-        _avg_time = ping_resp->resp_time;
-        
-
-        // Some debug info
-        DEBUG_PING(
-                "DEBUG: ping reply\n"
-                        "\ttotal_count = %d \n"
-                        "\tresp_time = %f ms\n"
-                        "\tseqno = %d \n"
-                        "\ttimeout_count = %d \n"
-                        "\tbytes = %d \n"
-                        "\ttotal_bytes = %d \n"
-                        "\ttotal_time = %f s\n"
-                        "\tping_err = %d \n",
-                ping_resp->total_count, ping_resp->resp_time, ping_resp->seqno,
-                ping_resp->timeout_count, ping_resp->bytes, ping_resp->total_bytes,
-                ping_resp->total_time, ping_resp->ping_err
-        );
-
-        // Is it time to end?
-        DEBUG_PING("Avg resp time %f ms\n", _avg_time);
-        
-        // Done, return to main function
-        esp_schedule();
-        
-        // just a check ...
-        if (_success + _errors != _expected_count) {
-            DEBUG_PING("Something went wrong: _success=%d and _errors=%d do not sum up to _expected_count=%d\n",_success, _errors, _expected_count );
-        }
+        esp_ping_handle_t ping;
+        esp_ping_new_session(&ping_config, &cbs, &ping);
+        esp_ping_start(ping);
     }
 
-    static inline byte _expected_count = 0;
-    static inline byte _errors = 0;
-    static inline byte _success = 0;
-    static inline float _avg_time = 0;
+    static void on_ping_success(esp_ping_handle_t hdl, void *args) {
+
+        uint8_t ttl;
+        uint16_t seqno;
+        uint32_t elapsed_time, recv_len;
+        ip_addr_t target_addr;
+        esp_ping_get_profile(hdl, ESP_PING_PROF_SEQNO, &seqno, sizeof(seqno));
+        esp_ping_get_profile(hdl, ESP_PING_PROF_TTL, &ttl, sizeof(ttl));
+        esp_ping_get_profile(hdl, ESP_PING_PROF_IPADDR, &target_addr, sizeof(target_addr));
+        esp_ping_get_profile(hdl, ESP_PING_PROF_SIZE, &recv_len, sizeof(recv_len));
+        esp_ping_get_profile(hdl, ESP_PING_PROF_TIMEGAP, &elapsed_time, sizeof(elapsed_time));
+        
+        // printf("%d bytes from %s icmp_seq=%d ttl=%d time=%d ms\n", recv_len, ipaddr_ntoa((ip_addr_t*)&target_addr), seqno, ttl, elapsed_time);
+
+    }
+
+    static void on_ping_timeout(esp_ping_handle_t hdl, void *args) {
+        uint16_t seqno;
+        ip_addr_t target_addr;
+        esp_ping_get_profile(hdl, ESP_PING_PROF_SEQNO, &seqno, sizeof(seqno));
+        esp_ping_get_profile(hdl, ESP_PING_PROF_IPADDR, &target_addr, sizeof(target_addr));
+        
+        // printf("From %s icmp_seq=%d timeout\n", ipaddr_ntoa((ip_addr_t*)&target_addr), seqno);
+        
+    }
+
+    static void on_ping_end(esp_ping_handle_t hdl, void *args) {
+        uint32_t transmitted;
+        uint32_t received;
+        uint32_t total_time_ms;
+
+        esp_ping_get_profile(hdl, ESP_PING_PROF_REQUEST, &transmitted, sizeof(transmitted));
+        esp_ping_get_profile(hdl, ESP_PING_PROF_REPLY, &received, sizeof(received));
+        esp_ping_get_profile(hdl, ESP_PING_PROF_DURATION, &total_time_ms, sizeof(total_time_ms));
+        printf("%d packets transmitted, %d received, time %dms\n", transmitted, received, total_time_ms);
+
+        esp_ping_delete_session(hdl);
+    }
+
+    static int get_netif_index()
+    {
+        struct netif *netif = netif_list;
+        while (netif) {
+            if (netif->name[0] == 's' && netif->name[1] == 't') { // "st" usually stands for STA
+                return netif_get_index(netif); // this gives the scope ID
+            }
+            netif = netif->next;
+        }
+        // Serial.println("No suitable network interface found for ping.");
+        return 0;
+    }
+
 
 };
 
-void pingTask(void *pvParameters) {
-    // This task is used to run the ping process
-    // It will be suspended until the ping process is done
-    while (true) {
-
-        ICMP::ping(IPAddress(192, 168, 2, 1)); // Example IPv4 address
-
-        const uint8_t ip[16] = { 0xfe, 0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-                                0x7c, 0xf5, 0x9c, 0x9d, 0x77, 0x7a, 0x27, 0xb7 };
-
-        ICMP::ping(IPv6Address(ip));
-
-        vTaskDelay(pdMS_TO_TICKS(1000));
-    }
-}
 
 #endif // ICMP_H
